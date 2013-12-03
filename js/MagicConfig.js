156097014a59b914205009ff87351149893f982e
stephenzhao <zfcyb2009@163.com>
2013-12-03 20:10:04 +0800

    cmbase

--- js/MagicConfig.js
+++ js/MagicConfig.js
@@ -1,13 +1,8 @@
 define(function(require, exports, module) {
 	//TODO configuration should be filtered by profile setting.
 	var magicConfig = {
-		cmsBase: "http://games.hoolai.com/cms",
-		// magicBase : "http://192.168.1.206:8181/order",
-		magicBase: "http://192.168.50.159:8080/lepao-order-web",
-//		 magicBase: "http://www.lepao.com", //声明：不要把自己本地的URL提交到svn
+		// magicBase: "http://127.0.1/lepao-order-web",
 		//FIXME 现在验证码 分属别的服务器
-		//			captchaBase:"http://192.168.50.160:8080/portal"
-		//			captchaBase:"http://192.168.1.206:8181/order"
 		targetUrl: {
 			cart: "cart.html",
 			userBuyInfo: "userbuyinfo.html",
