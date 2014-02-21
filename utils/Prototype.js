c5e2bd093c134f21553839c0842d10bb25d5bc76
stephenzhao <zfcyb2009@163.com>
2014-02-21 15:25:38 +0800

    继承

--- utils/Prototype.js
+++ utils/Prototype.js
@@ -38,7 +38,6 @@ var article = new Article();
 //via classical pattern #1
 function BlogPost() {};
 BlogPost.prototype = article;
-var blog = new BlogPost();
 //以上代码不需要new Article , 因为可用的实例
 
 //static page (静态)继承了article
@@ -46,8 +45,78 @@ var blog = new BlogPost();
 function StaticPage() {
 	Article.call(this);
 }
-var page = new StaticPage();
+
+var blog = new BlogPost();
 blog.hasOwnProperty('tags'); //false
-blog.tags.push("html");//article 的tags 会更新
-page.hasOwnProperty('tags');//true
-page.tags.push("php");//article 的tags 不会更新
+
+var page = new StaticPage();
+page.hasOwnProperty('tags'); //true
+
+///直接继承prototype
+1 / Cat.prototype = new Animal(); //将Cat的原型对象指向Animal的实例，就能继承animal了
+2 / Cat.prototype = Animal.prototype; //更加节省内存 问题在于 两个实例指向同一个对象 任何对Cat.prototype的修改，都会反映到Animal.prototype
+
+　　
+Cat.prototype.constructor = Cat; //为新的prototype对象加上constructor属性，并将这个属性指回原来的构造函数 
+　　
+var cat1 = new Cat("大毛", "黄色");　　
+alert(cat1.species); // 动物
+///利用空对象作为中介
+var F = function() {};
+F.prototype = Animal.prototype;
+Cat.prototype = new F();
+Cat.constructor = Cat;
+
+//继承的封装
+function extend(Child, Parent) {
+	var F = function() {};
+	F.prototype = Parent.prototype;
+	Child.prototype = new F();
+	Child.prototype.constructor = Child;
+	Child.uber = Parent.prototype; //备选属性
+}
+// 拷贝继承
+function extend2(Child, Parent) {
+	var C = Child.prototype;
+	var P = Parent.prototype;
+	for (var i in P) {
+		C[i] = P[i];
+	}
+	C.uber = P;
+}
+
+//非构造函数继承
+　
+//object 方式
+function object(o) {　　
+	function F() {}　　
+	F.prototype = o;　　
+	return new F();
+}
+
+var Child = object(Parent);
+Child.name = "haha";
+
+//浅拷贝 早起 jquery做法 但是 o的属性是一个数组或者对象，呢么c得到的只是一个内存地址，对c操作，o也会改变
+function extendCopy(o) {
+	var c = {};
+	for (i in o) {
+		c[i] = o[i];
+	}
+	c.uber = o;
+	return c;
+}
+
+// 深拷贝 改进浅拷贝
+
+function deepCopy(P,C){
+	var c = c || {};
+	for(var i in P){
+		if(typeof P[i]){
+			C[i] = (P[i].constructor === Array())?[]:{};
+			deepCopy(P[i],C[i]);
+		}else{
+			C[i] = P[i];
+		}
+	}
+}
